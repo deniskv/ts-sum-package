b91fe98e51374f27bd1030c5f8d7203d7e93c486
Denis Kovalev <deniskovalev4791@gmail.com>
2026-03-27 15:30:39 +0100

    test: basic CI check

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@
  * @param b - Second number
  * @returns The sum of a and b
  */
+/**
+*Comments for pipline test
+* test 1: lint, build, unit tests, lockfile check, linear history, branch up-to-date.
+*/
 export function sum(a: number, b: number): number {
   return a + b;
 }
